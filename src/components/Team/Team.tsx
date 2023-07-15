97d41425f4bef085c61da4fcde9b939c7bbddc6d
whizzie99 <wisdomobiomadev@gmail.com>
2023-07-15 20:29:30 +0100

    add animations to team section component

--- src/components/Team/Team.tsx
+++ src/components/Team/Team.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import gsap from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
 import Container from "../shared/Container/Container";
 import GradientCircle from "../shared/GradientCircle/GradientCircle";
 import { FaLinkedin, FaEnvelope } from "react-icons/fa6";
@@ -14,21 +16,76 @@ import {
 
 import sample from "../../assets/images/team-1.jpeg";
 
+gsap.registerPlugin(ScrollTrigger);
+
+
 const Team = () => {
+
+  const sectionRef = useRef<HTMLDivElement>(null);
+  const elementsRef = useRef<HTMLElement[]>([]);
+
+  const addElementRef = (element: HTMLElement | null) => {
+    if (element) {
+      elementsRef.current.push(element);
+    }
+  };
+
+  useEffect(() => {
+    const sectionElement = sectionRef.current;
+    const elements = elementsRef.current;
+
+    const tl = gsap.timeline();
+
+    elements.forEach((element) => {
+      gsap.set(element, { opacity: 0, y: 50 });
+      tl.to(element, { opacity: 1, y: 0, duration: 0.6, ease: "back.out(2)", stagger: {amount: 0.5} });
+    });
+
+    const sectionId = sectionElement?.id;
+
+    const scrollTrigger = ScrollTrigger.create({
+      trigger: sectionElement,
+      start: "top 80%",
+      end: "bottom 20%",
+      scrub: true,
+      onEnter: () => {
+        tl.restart();
+      },
+      onEnterBack: () => {
+        tl.restart();
+      },
+      onLeave: () => {
+        tl.progress(0).pause();
+      },
+      onLeaveBack: () => {
+        tl.progress(0).pause();
+      },
+    });
+
+    if (sectionId) {
+      ScrollTrigger.getById(sectionId)?.kill();
+    }
+
+    return () => {
+      tl.kill();
+      scrollTrigger.kill();
+    };
+  }, []);
+
   return (
-    <StyledWrapper>
+    <StyledWrapper ref={sectionRef}>
       <Container>
         <StyledHeading>
           <div>
-            <h2>meet the team</h2>
+            <h2 ref={addElementRef}>meet the team</h2>
             <GradientCircle />
           </div>
-          <p>
+          <p ref={addElementRef}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, illo.
           </p>
         </StyledHeading>
         <StyledTeamGrid>
-          <StyledTeamCard>
+          <StyledTeamCard ref={addElementRef}>
             <StyledCardImg>
               <img src={sample} alt="" />
             </StyledCardImg>
@@ -48,7 +105,7 @@ const Team = () => {
               </StyledContactIcons>
             </StyledCardContent>
           </StyledTeamCard>
-          <StyledTeamCard>
+          <StyledTeamCard ref={addElementRef}>
             <StyledCardImg>
               <img src={sample} alt="" />
             </StyledCardImg>
@@ -68,7 +125,7 @@ const Team = () => {
               </StyledContactIcons>
             </StyledCardContent>
           </StyledTeamCard>
-          <StyledTeamCard>
+          <StyledTeamCard ref={addElementRef}>
             <StyledCardImg>
               <img src={sample} alt="" />
             </StyledCardImg>
